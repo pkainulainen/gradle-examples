1fd75e093d507d3ad502834296f65dc5920b1060
Petri Kainulainen <petri.kainulainen@gmail.com>
2014-06-22 11:43:35 +0300

    First commit

---
+++ first-java-project/src/main/java/net/petrikainulainen/gradle/HelloWorld.java
@@ -0,0 +1,11 @@
+package net.petrikainulainen.gradle;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
